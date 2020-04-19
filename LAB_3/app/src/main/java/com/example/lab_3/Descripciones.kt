e0aabf2b8286f66c4a6f03a79974964e064bd237
DiaZa13 <cor191025@uvg.edu.gt>
2020-04-19 00:16:41 -0600

    Creacion y diseño de Activity2

---
+++ LAB_3/app/src/main/java/com/example/lab_3/Descripciones.kt
@@ -0,0 +1,12 @@
+package com.example.lab_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Descripciones : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_descripciones)
+    }
+}
